e9fd5c8cd45472b98d18f4511bb76f202ba164d4
axiomasham <axio@axio.name>
2015-10-02 10:23:27 +0000

    Added timewalking events - I hope it will work, blizz should fix the calendar Link for seal was removed from LDB plugin text

--- RaidCurrencyReminder.lua
+++ RaidCurrencyReminder.lua
@@ -1,17 +1,20 @@
 ﻿---------------------
 ----- Constants -----
 ---------------------
-local sealCurrencyID = 1129;
-local maxSealsFromQuests = 3;
-local maxSealsPerCharacter = 10;
-local sealTexture = [[Interface\Icons\achievement_battleground_templeofkotmogu_02_green]];
-local sealTextureBW = [[Interface\AddOns\RaidCurrencyReminder\media\achievement_battleground_templeofkotmogu_02_green_bw.tga]];
-local intervalBetweenPeriodicNotifications = 900;
+local SEAL_CURRENCY_ID = 1129;
+local MAX_SEALS_FROM_QUESTS = 3;
+local MAX_SEALS_PER_CHARACTER = 10;
+local MIN_CHARACTER_LEVEL_REQUIRED = 100;
+local SEAL_TEXTURE = [[Interface\Icons\achievement_battleground_templeofkotmogu_02_green]];
+local SEAL_TEXTURE_BW = [[Interface\AddOns\RaidCurrencyReminder\media\achievement_battleground_templeofkotmogu_02_green_bw.tga]];
+local SEAL_LINK = GetCurrencyLink(SEAL_CURRENCY_ID);
+local INTERVAL_BETWEEN_PERIODIC_NOTIFICATIONS = 900;
 ---------------------
 ---------------------
 
 local LDBPlugin;
-local LastAvailableSealsAmount = 0;
+local LastAvailableSealsAmount = -1;
+local CalendarOpened = false;
 
 local quests = {
 	36054, -- // gold
@@ -29,12 +32,17 @@ local quests = {
 	36058, -- // Bunker/Mill
 };
 
+local holidayEvents = {
+	["calendar_weekendburningcrusade"] = 	 39020,
+	["calendar_weekendwrathofthelichking"] = 39021,
+};
+
 local function Print(...)
 	local text = "";
 	for i = 1, select("#", ...) do
 		text = text..tostring(select(i, ...)).." "
 	end
-	DEFAULT_CHAT_FRAME:AddMessage(format("%s", text), 1, 0.5, 0);
+	DEFAULT_CHAT_FRAME:AddMessage(text, 1, 0.5, 0);
 end
 
 local function PlayerOwnsBunker()
@@ -42,7 +50,7 @@ local function PlayerOwnsBunker()
 	if (t ~= nil and #t > 0) then
 		for _, value in pairs(t) do
 			if (value.size == 3) then
-				local buildingID, buildingName, _, _, _, rank = C_Garrison.GetOwnedBuildingInfo(value.id);
+				local buildingID = C_Garrison.GetOwnedBuildingInfo(value.id);
 				if (buildingID == 10) then
 					return true;
 				end
@@ -52,64 +60,110 @@ local function PlayerOwnsBunker()
 	return false;
 end
 
-local function PrintInfo(amount)
+local function GetNumAvailableSeals()
+	local numQuestsAvailable = MAX_SEALS_FROM_QUESTS;
+	for _, questID in pairs(quests) do
+		if (IsQuestFlaggedCompleted(questID)) then
+			numQuestsAvailable = numQuestsAvailable - 1;
+		end
+	end
+	if (numQuestsAvailable < 0) then
+		Print("RCR: MAX_SEALS_FROM_QUESTS has incorrect value!");
+		numQuestsAvailable = 0;
+	end
+	
+	local _, _, day = CalendarGetDate();
+	for eventIndex = 1, CalendarGetNumDayEvents(0, day) do
+		local _, _, _, calendarType, _, _, texture = CalendarGetDayEvent(0, day, eventIndex);
+		if (calendarType == "HOLIDAY") then
+			local questID = holidayEvents[texture];
+			if (questID ~= nil) then
+				if (not IsQuestFlaggedCompleted(questID)) then
+					return numQuestsAvailable, 1;
+				end
+			end
+		end
+	end
+	return numQuestsAvailable, 0;
+end
+
+local function GetNumObtainableSeals()
+	if (UnitLevel("player") >= MIN_CHARACTER_LEVEL_REQUIRED) then
+		local _, amount = GetCurrencyInfo(SEAL_CURRENCY_ID);
+		local amountFromRegularQuests, amountFromHoliday = GetNumAvailableSeals();
+		return min(amountFromRegularQuests + amountFromHoliday, MAX_SEALS_PER_CHARACTER - amount);
+	else
+		return 0;
+	end
+end
+
+local function PrintInfo()
 	Print("-----------------------------------");
-	Print("You can buy "..tostring(amount).." "..GetCurrencyLink(sealCurrencyID));
+	local numFromQuests, numFromHoliday = GetNumAvailableSeals();
+	if (numFromQuests > 0) then
+		Print(format("You can buy %s %s", numFromQuests, SEAL_LINK));
+	end
+	if (numFromHoliday > 0) then
+		Print(format("You can get %s %s from holiday event", numFromHoliday, SEAL_LINK));
+	end
 	if (PlayerOwnsBunker() and not IsQuestFlaggedCompleted(36058)) then
 		Print("Don't forget about Bunker/Mill in your garrison!");
 	end
 	Print("-----------------------------------");
 end
 
-local function UpdatePlugin(amount)
+local function UpdatePlugin()
 	if (LDBPlugin ~= nil) then
-		if (amount == 0) then
+		local numObtainable = GetNumObtainableSeals();
+		local numFromQuests, numFromHoliday = GetNumAvailableSeals();
+		if (numObtainable == 0) then
 			LDBPlugin.text = nil;
-			LDBPlugin.icon = sealTextureBW;
-			LDBPlugin.OnTooltipShow = function(tooltip)
-				tooltip:AddLine("Raid Currency Reminder");
-				tooltip:AddLine(" ");
-				tooltip:AddLine("You have already bought all possible "..GetCurrencyLink(sealCurrencyID));
-			end;
+			LDBPlugin.icon = SEAL_TEXTURE_BW;
 		else
-			LDBPlugin.text = tostring(amount).." "..GetCurrencyLink(sealCurrencyID);
-			LDBPlugin.icon = sealTexture;
-			LDBPlugin.OnTooltipShow = function(tooltip)
-				tooltip:AddLine("Raid Currency Reminder");
-				tooltip:AddLine(" ");
-				tooltip:AddLine("You can buy "..tostring(amount).." "..GetCurrencyLink(sealCurrencyID));
-			end;
+			if (numFromHoliday > 0) then
+				LDBPlugin.text = format("Seals: %s|cff228b22+%s|r", numFromQuests, numFromHoliday);
+			else
+				LDBPlugin.text = format("Seals: %s", numFromQuests);
+			end
+			LDBPlugin.icon = SEAL_TEXTURE;
 		end
+		LDBPlugin.OnTooltipShow = function(tooltip)
+			tooltip:AddLine("Raid Currency Reminder");
+			tooltip:AddLine(" ");
+			if (numFromQuests > 0) then
+				tooltip:AddLine(format("You can buy %s %s in Ashran", numFromQuests, SEAL_LINK));
+			end
+			if (numFromHoliday > 0) then
+				tooltip:AddLine(format("You can get %s %s from holiday event", numFromHoliday, SEAL_LINK));
+			end
+			if ((numFromHoliday + numFromQuests) == 0) then
+				tooltip:AddLine("You have already got all possible "..SEAL_LINK);
+			end
+		end;
 	end
-end
-
-local function GetAvailableSeals()
-	local _, amount = GetCurrencyInfo(sealCurrencyID);
-	local possible = min(maxSealsFromQuests, maxSealsPerCharacter - amount);
-	local numQuestsAvailable = maxSealsFromQuests;
-	for _, questID in pairs(quests) do
-		if (IsQuestFlaggedCompleted(questID)) then
-			numQuestsAvailable = numQuestsAvailable - 1;
-		end
-	end
-	return min(numQuestsAvailable, possible);
 end
 
 local function LOADING_SCREEN_DISABLED()
-	local availableSeals = GetAvailableSeals();
-	UpdatePlugin(availableSeals);
-	if (availableSeals > 0) then
+	UpdatePlugin();
+	if (GetNumObtainableSeals() > 0) then
 		C_Timer.After(3, function()
-			PrintInfo(availableSeals);
+			PrintInfo();
 		end);
+	end
+	if (not CalendarOpened) then
+		C_Timer.After(1.0, function()
+			GameTimeFrame_OnClick(GameTimeFrame);
+			GameTimeFrame_OnClick(GameTimeFrame);
+		end);
+		CalendarOpened = true;
 	end
 end
 
 local function eFrame_OnElapsed()
-	local availableSeals = GetAvailableSeals();
-	if (LastAvailableSealsAmount ~= availableSeals) then
-		UpdatePlugin(availableSeals);
-		LastAvailableSealsAmount = availableSeals;
+	local obtainableSeals = GetNumObtainableSeals();
+	if (LastAvailableSealsAmount ~= obtainableSeals) then
+		UpdatePlugin();
+		LastAvailableSealsAmount = obtainableSeals;
 	end
 end
 
@@ -135,18 +189,17 @@ if (ldb ~= nil) then
 		{
 			type = "data source",
 			text = "N/A",
-			icon = sealTexture,
+			icon = SEAL_TEXTURE,
 			tocname = "RaidCurrencyReminder",
 		}
 	);
 end
 
 local function OnTimerElapsed()
-	local availableSeals = GetAvailableSeals();
-	if (availableSeals > 0) then
-		PrintInfo(availableSeals);
+	if (GetNumObtainableSeals() > 0) then
+		PrintInfo();
 	end
-	C_Timer.After(intervalBetweenPeriodicNotifications, OnTimerElapsed);
+	C_Timer.After(INTERVAL_BETWEEN_PERIODIC_NOTIFICATIONS, OnTimerElapsed);
 end
 
-C_Timer.After(intervalBetweenPeriodicNotifications, OnTimerElapsed);
+C_Timer.After(INTERVAL_BETWEEN_PERIODIC_NOTIFICATIONS, OnTimerElapsed);
