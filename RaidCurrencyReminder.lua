fa2522cbb1620f74c26f74af2a58f08bb2455b67
axiomasham <axiomasham@e072ffdd-aceb-426e-b220-c70fb3962d08>
2016-11-09 09:16:54 +0000

    Added "A Frozen Path Through Time" quest

--- RaidCurrencyReminder.lua
+++ RaidCurrencyReminder.lua
@@ -13,14 +13,17 @@ local MIN_INTERVAL_BETWEEN_PRINTS = 30;
 ---------------------
 ---------------------
 
-local ShouldDeductOneSeal;
-
 local LDBPlugin;
 local LastAvailableSealsAmount = -1;
 local CalendarOpened = false;
 local LastTimePrint = 0;
-local DisablePrintUntilReload = false;
 local db;
+local GotNewSeal = false;
+
+RaidCurrencyReminderDB = RaidCurrencyReminderDB or { };
+local LocalPlayerFullName = UnitName("player").." - "..GetRealmName();
+local INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS = 3600;
+local LastTimerPopupDisplayed;
 
 local quests = {
 	43892,	-- // 1000 class hall
@@ -33,9 +36,13 @@ local quests = {
 
 local holidayEvents = {
 	["calendar_weekendburningcrusade"] = 	 44164,
-	-- ["calendar_weekendwrathofthelichking"] = 39021,
+	["calendar_weekendwrathofthelichking"] = 44166,
 	-- ["calendar_weekendcataclysm"] = 		 40792,
 };
+
+---------------------
+------- Utils -------
+---------------------
 
 local function ColorizeText(text, r, g, b)
 	return format("|cff%02x%02x%02x%s|r", r*255, g*255, b*255, text);
@@ -56,6 +63,33 @@ local function CompareDates(hourA, minuteA, hourB, minuteB)
 		return true;
 	else
 		return false;
+	end
+end
+
+local function DaysFromCivil(y, m, d)
+	if (m <= 2) then
+		y = y - 1;
+		m = m + 9;
+    else
+		m = m - 3;
+    end
+    local era = math.floor(y/400);
+    local yoe = y - era * 400;     		                                   	-- [0, 399]
+    local doy = math.modf((153*m + 2)/5) + d-1;              		     	-- [0, 365]
+    local doe = yoe * 365 + math.modf(yoe/4) - math.modf(yoe/100) + doy;	-- [0, 146096]
+    return era * 146097 + doe - 719468;
+end
+
+---------------------
+---------------------
+
+local function ShouldDeductOneSeal()
+	local _, month, day, year = CalendarGetDate();
+	local daysFromCivil = DaysFromCivil(year, month, day);
+	if (daysFromCivil - db.LastDateAskedAboutClassOrderHall > 5) then
+		return false;
+	else
+		return true;
 	end
 end
 
@@ -104,20 +138,6 @@ local function GetNumObtainableSeals()
 	end
 end
 
-local function PrintInfo()
-	if (not DisablePrintUntilReload) then
-		Print("-----------------------------------");
-		local numFromQuests, numFromHoliday = GetNumAvailableSeals();
-		if (numFromQuests > 0) then
-			Print(format("You can buy %s %s", numFromQuests, SEAL_LINK));
-		end
-		if (numFromHoliday > 0) then
-			Print(format("You can get %s %s from holiday event", numFromHoliday, SEAL_LINK));
-		end
-		Print("-----------------------------------");
-	end
-end
-
 local function UpdatePlugin()
 	if (LDBPlugin ~= nil) then
 		local numObtainable = GetNumObtainableSeals();
@@ -147,29 +167,7 @@ local function UpdatePlugin()
 			end
 			tooltip:AddLine(" ");
 			tooltip:AddLine("|cffeda55fLeftClick:|r open currencies tab");
-			if (DisablePrintUntilReload) then
-				tooltip:AddLine(ColorizeText("Chat notifications are disabled for this game session", 1, 0, 0));
-			else
-				tooltip:AddLine("|cffeda55fRightClick:|r disable chat notifications for this session");
-			end
 		end;
-	end
-end
-
-local function LOADING_SCREEN_DISABLED()
-	UpdatePlugin();
-	if (GetTime() - LastTimePrint > MIN_INTERVAL_BETWEEN_PRINTS and GetNumObtainableSeals() > 0) then
-		C_Timer.After(3, function()
-			-- PrintInfo();
-		end);
-		LastTimePrint = GetTime();
-	end
-	if (not CalendarOpened) then
-		C_Timer.After(1.0, function()
-			GameTimeFrame_OnClick(GameTimeFrame);
-			GameTimeFrame_OnClick(GameTimeFrame);
-		end);
-		CalendarOpened = true;
 	end
 end
 
@@ -183,36 +181,6 @@ local function GetNumQuestsCompleted()
 	return num;
 end
 
-local function eFrame_OnElapsed()
-	local obtainableSeals = GetNumObtainableSeals();
-	if (LastAvailableSealsAmount ~= obtainableSeals) then
-		UpdatePlugin();
-		LastAvailableSealsAmount = obtainableSeals;
-	end
-	
-	local numQuestsCompleted = GetNumQuestsCompleted();
-	if (numQuestsCompleted < db.QuestsCompleted) then -- // seems like it's next raid week now
-		db.LastDateAskedAboutClassOrderHall = 0;
-	end
-	db.QuestsCompleted = numQuestsCompleted;
-end
-
-local eFrame = CreateFrame("frame");
-eFrame.elapsed = 0;
-eFrame:RegisterEvent("LOADING_SCREEN_DISABLED");
-eFrame:SetScript("OnEvent", function(this, event, ...)
-	if (event == "LOADING_SCREEN_DISABLED") then
-		LOADING_SCREEN_DISABLED();
-	end
-end);
-eFrame:SetScript("OnUpdate", function(this, elapsed)		-- // -------------------------------------------------------------------------------------------------------------------------
-	this.elapsed = this.elapsed + elapsed;					-- // -------------------------------------------------------------------------------------------------------------------------
-	if (this.elapsed >= 1.0) then							-- // CURRENCY_DISPLAY_UPDATE, QUEST_TURNED_IN don't work. IsQuestFlaggedCompleted returns irrelevant info during this events.
-		eFrame_OnElapsed();									-- // So I'm using timer to solve it.
-		this.elapsed = 0;									-- // -------------------------------------------------------------------------------------------------------------------------
-	end														-- // -------------------------------------------------------------------------------------------------------------------------
-end);														-- // -------------------------------------------------------------------------------------------------------------------------
-
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1", true);
 if (ldb ~= nil) then
 	LDBPlugin = ldb:NewDataObject("Raid Currency Reminder",
@@ -224,45 +192,10 @@ if (ldb ~= nil) then
 		}
 	);
 	LDBPlugin.OnClick = function(display, button)
-		if (button == "RightButton" and not DisablePrintUntilReload) then
-			DisablePrintUntilReload = true;
-			Print("Chat notifications are disabled for this game session");
-		end
 		if (button == "LeftButton") then
 			ToggleCharacter("TokenFrame");
 		end
 	end
-end
-
-local function OnTimerElapsed()
-	if (GetTime() - LastTimePrint > MIN_INTERVAL_BETWEEN_PRINTS and GetNumObtainableSeals() > 0) then
-		-- PrintInfo();
-		LastTimePrint = GetTime();
-	end
-	C_Timer.After(INTERVAL_BETWEEN_PERIODIC_CHAT_NOTIFICATIONS, OnTimerElapsed);
-end
-
-C_Timer.After(INTERVAL_BETWEEN_PERIODIC_CHAT_NOTIFICATIONS, OnTimerElapsed);
-
-
-
-RaidCurrencyReminderDB = RaidCurrencyReminderDB or { };
-local LocalPlayerFullName = UnitName("player").." - "..GetRealmName();
-
-local INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS = 3600;
-
-local function DaysFromCivil(y, m, d)
-	if (m <= 2) then
-		y = y - 1;
-		m = m + 9;
-    else
-		m = m - 3;
-    end
-    local era = math.floor(y/400);
-    local yoe = y - era * 400;     		                                   	-- [0, 399]
-    local doy = math.modf((153*m + 2)/5) + d-1;              		     	-- [0, 365]
-    local doe = yoe * 365 + math.modf(yoe/4) - math.modf(yoe/100) + doy;	-- [0, 146096]
-    return era * 146097 + doe - 719468;
 end
 
 local function msg(text)
@@ -300,49 +233,6 @@ local function msgWithQuestion(text, funcOnAccept, funcOnCancel)
   StaticPopup_Show(frameName);
 end
 
-function ShouldDeductOneSeal()
-	local _, month, day, year = CalendarGetDate();
-	local daysFromCivil = DaysFromCivil(year, month, day);
-	if (daysFromCivil - db.LastDateAskedAboutClassOrderHall > 5) then
-		return false;
-	else
-		return true;
-	end
-end
-
-local function NewChecker()
-	if (IsResting()) then
-		local numObtainable = GetNumObtainableSeals();
-		local numFromQuests, numFromHoliday = GetNumAvailableSeals();
-		if (numObtainable > 0) then
-			if (numFromQuests == 1) then
-				if (not ShouldDeductOneSeal()) then
-					msgWithQuestion(format(
-						"You can still get 1 %s\n\n" ..
-						"Unfortunately, RCR can't determine if you got seal in your class order hall. If you haven't corresponding class order hall advancement, you can get seal from Archmage Lan'dalock in Dalaran\n\n" ..
-						"Have you got seal from work order this week?", SEAL_LINK),
-						function()
-							local _, month, day, year = CalendarGetDate();
-							db.LastDateAskedAboutClassOrderHall = DaysFromCivil(year, month, day);
-						end,
-						function() end);
-				end
-			elseif (numFromQuests > 1) then
-				msg(format("You can still get %s %s from Archmage Lan'dalock in Dalaran", numFromQuests, SEAL_LINK));
-			end
-			if (numFromHoliday > 0) then
-				msg(format("You can still get one %s from weekly event\nVisit Archmage Timear in Dalaran to start quest", SEAL_LINK));
-			end
-			db.LastTimeChecked = GetTime();
-			local _, month, day, year = CalendarGetDate();
-			db.LastDateChecked = tostring(year) .. tostring(month) .. tostring(day);
-		end
-		C_Timer.After(INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS, NewChecker);
-	else
-		C_Timer.After(15, NewChecker);
-	end
-end
-
 local function InitializeDB()
 	local defaults = {
 		LastDateAskedAboutClassOrderHall = 0,
@@ -361,20 +251,113 @@ local function InitializeDB()
 	db = RaidCurrencyReminderDB[LocalPlayerFullName];
 end
 
-local newFrame = CreateFrame("frame");
-newFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
-newFrame:SetScript("OnEvent", function(self, event, ...)
-	InitializeDB();
-	local _, month, day, year = CalendarGetDate();
-	local currentDate = tostring(year) .. tostring(month) .. tostring(day);
-	if (currentDate ~= db.LastDateChecked) then
-		C_Timer.After(10, NewChecker);
-	else
-		if (GetTime() - db.LastTimeChecked > INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS) then
-			C_Timer.After(10, NewChecker);
-		else
-			C_Timer.After(INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS - GetTime() + db.LastTimeChecked, NewChecker);
+local function ShowPopupAboutMissingSeals()
+	local numObtainable = GetNumObtainableSeals();
+	local numFromQuests, numFromHoliday = GetNumAvailableSeals();
+	if (numObtainable > 0) then
+		if (numFromQuests == 1) then
+			if (not ShouldDeductOneSeal()) then
+				msgWithQuestion(format(
+					"You can still get 1 %s\n\n" ..
+					"Unfortunately, RCR can't determine if you got seal in your class order hall. If you haven't corresponding class order hall advancement, you can get seal from Archmage Lan'dalock in Dalaran\n\n" ..
+					"Have you got seal from work order this week?", SEAL_LINK),
+					function()
+						local _, month, day, year = CalendarGetDate();
+						db.LastDateAskedAboutClassOrderHall = DaysFromCivil(year, month, day);
+					end,
+					function() end);
+			end
+		elseif (numFromQuests > 1) then
+			msg(format("You can still get %s %s from Archmage Lan'dalock in Dalaran", numFromQuests, SEAL_LINK));
+		end
+		if (numFromHoliday > 0) then
+			msg(format("You can still get one %s from weekly event\nVisit Archmage Timear in Dalaran to start quest", SEAL_LINK));
+		end
+		db.LastTimeChecked = GetTime();
+		local _, month, day, year = CalendarGetDate();
+		db.LastDateChecked = tostring(year) .. tostring(month) .. tostring(day);
+	end
+end
+
+local function ShowPopupAboutUnknownSeal()
+	local numObtainable = GetNumObtainableSeals();
+	local numFromQuests, numFromHoliday = GetNumAvailableSeals();
+	if (numObtainable > 0 and numFromQuests > 0 and not ShouldDeductOneSeal()) then
+		msgWithQuestion(format(
+			"You have just got seal from unknown source\n\n" ..
+			"Unfortunately, RCR can't determine if you got seal in your class order hall. If you haven't corresponding class order hall advancement, you can get seal from Archmage Lan'dalock in Dalaran\n\n" ..
+			"Have you got this seal from work order?", numObtainable, SEAL_LINK),
+			function()
+				local _, month, day, year = CalendarGetDate();
+				db.LastDateAskedAboutClassOrderHall = DaysFromCivil(year, month, day);
+				UpdatePlugin();
+			end,
+			function() end);
+	end
+end
+
+local function CheckInfo()
+	local numQuestsCompleted = GetNumQuestsCompleted();
+	if (GotNewSeal) then
+		GotNewSeal = false;
+		if (db.QuestsCompleted ~= numQuestsCompleted) then -- // we have completed quest in Dalaran
+			UpdatePlugin();
+			print("we have completed quest in Dalaran");
+		else -- // seems like we got seal from work order or mission
+			ShowPopupAboutUnknownSeal();
+			print("seems like we got seal from work order or mission");
 		end
 	end
-	self:UnregisterEvent("PLAYER_ENTERING_WORLD");
+	local obtainableSeals = GetNumObtainableSeals();
+	if (LastAvailableSealsAmount ~= obtainableSeals) then
+		UpdatePlugin();
+		LastAvailableSealsAmount = obtainableSeals;
+	end
+	if (numQuestsCompleted < db.QuestsCompleted) then -- // seems like it's next raid week now
+		db.LastDateAskedAboutClassOrderHall = 0;
+		UpdatePlugin();
+	end
+	if (obtainableSeals > 0) then
+		if (GetTime() - LastTimerPopupDisplayed > INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS) then
+			if (IsResting()) then
+				ShowPopupAboutMissingSeals();
+				LastTimerPopupDisplayed = GetTime();
+			end
+		end
+	end
+	--print(format("Quests completed: %s; DB quest completed: %s; ObtainableSeals: %s; GetNumAvailableSeals: %s; ShouldDeductOneSeal: %s", numQuestsCompleted, db.QuestsCompleted, obtainableSeals, GetNumAvailableSeals(), tostring(ShouldDeductOneSeal())));
+	db.QuestsCompleted = numQuestsCompleted;
+	C_Timer.After(1.0, CheckInfo);
+end
+
+local newFrame = CreateFrame("frame");
+newFrame:RegisterEvent("LOADING_SCREEN_DISABLED");
+newFrame:SetScript("OnEvent", function(self, event, ...)
+	if (event == "LOADING_SCREEN_DISABLED") then
+		InitializeDB();
+		self:UnregisterEvent("LOADING_SCREEN_DISABLED");
+		self:RegisterEvent("CHAT_MSG_CURRENCY");
+		UpdatePlugin();
+		if (not CalendarOpened) then
+			C_Timer.After(1.0, function()
+				GameTimeFrame_OnClick(GameTimeFrame);
+				GameTimeFrame_OnClick(GameTimeFrame);
+			end);
+			CalendarOpened = true;
+		end
+		local _, month, day, year = CalendarGetDate();
+		local currentDate = tostring(year) .. tostring(month) .. tostring(day);
+		if (currentDate ~= db.LastDateChecked) then
+			LastTimerPopupDisplayed = -INTERVAL_BETWEEN_PERIODIC_POPUP_NOTIFICATIONS;
+		else
+			LastTimerPopupDisplayed = db.LastTimeChecked;
+		end
+		C_Timer.After(1.0, CheckInfo);
+	elseif (event == "CHAT_MSG_CURRENCY") then
+		local msg = ...;
+		Print(msg, ":::::", msg:find(SEAL_LINK, 1, true));
+		if (msg:find(SEAL_LINK, 1, true)) then
+			C_Timer.After(1.0, function() GotNewSeal = true; end);
+		end
+	end
 end);
